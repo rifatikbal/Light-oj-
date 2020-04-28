c6ba363cb872349a6137b85cf6525fc93cca328a
Rifat ikbal <ikhossain991@gmail.com>
2020-04-29 04:53:30 +0600

    Add files via upload

---
+++ 1030.cpp
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <iostream>
+#include <climits>
+#include <map>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <vector>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <utility>
+#include <queue>
+ 
+using namespace std;
+ 
+#define ll long long
+#define ulli  unsigned ll int
+#define sl(n) scanf("%lld", &n)
+#define sl2(a,b) scanf("%lld%lld", &a,&b)
+#define sl3(a,b,c) scanf("%lld%lld%lld", &a,&b,&c)
+#define sl(n) scanf("%lld", &n)
+#define sf(n) scanf("%lf", &n)
+#define si(n) scanf("%d", &n)
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define plp pair <int, pll >
+#define pb push_back
+#define mkpr  make_pair
+#define   ff   first
+#define   ss  second
+#define  srt(V)  sort(V.begin(),V.end());
+#define nl printf("\n");
+#define  PI  2*acos(0.0);
+#define  INFP 100000000000000000
+#define  INFN (0-(ll)1<<62)
+#define  loop(a,b)  for(int g=a;g<=b;g++)
+#define  for1(n)  for(int i=0;i<n;i++)
+#define  for2(n)  for(int j=0;j<n;j++)
+#define  for3(n)  for(int k=0;k<n;k++)
+#define  for4(n)  for(int l=0;l<n;l++)
+#define  tst(n)  for(int cs=1;cs<=n;cs++)
+#define spc <<" "<<
+#define  pys puts("YES");
+#define  pno  puts("NO");
+#define bug puts("bug");
+#define bug1(a) cout<<a<<endl;
+ 
+#define bug2(a,b) cout<<a<<" "<<b<<endl;
+#define bug3(a,b,c) cout<<a<<" "<<b<<" "<<c<<endl;
+/*
+#define bug4(a,b,c,d) cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+ 
+#define bug5(a,b,c,d,e) cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<endl;
+#define bug5(a,b,c,d,e,f) cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<endl;
+#define bug1(a) cout<<a<<endl;*/
+#define  pmn puts("-1");
+#define zero puts("0");
+#define limit 200006
+ 
+ 
+typedef pair<ll,ll> iPair;
+ 
+ 
+ double ans[101];
+ int A[101];
+ 
+int main()
+{
+ 
+   /*            
+       freopen("input.txt","r",stdin);
+       freopen("output.txt","w",stdout);*/
+ 
+                int test;
+                cin>>test;
+                tst(test)
+                {
+ 
+ 
+ 
+                    int  n;
+ 
+                    si(n);
+ 
+                    for(ll i=1;i<=n;i++)si(A[i]),ans[i]=0;
+ 
+ 
+ 
+ 
+                     ans[n]=0;
+                    for(ll i=n-1;i>0;i--)
+                    {
+ 
+                         
+                         ll cnt=0;
+ 
+                         //bug
+ 
+                        for(ll j=i+1;j<=n&&cnt<6;j++)
+                        {
+                         ans[i]+=(ans[j]+A[j]);
+ 
+ 
+                      //   cout<<A[j]<<" "<<ans[j]<<endl;
+ 
+                         cnt++;
+ 
+                        }
+ 
+                        if(cnt)
+ 
+ 
+                        ans[i]/=cnt;
+ 
+                    //cout<<i<<" "<<ans[i]<<" "<<cnt<<endl;
+                    }
+ 
+                 printf("Case %d: %0.6f\n",cs,(double)ans[1]+A[1]);
+ 
+ 
+ 
+ 
+           
+           
+           
+           
+                   
+                }
+       
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+        /**
+       *    @author     : Ikbal Hossain
+       *    @University  : RUET CSE 15
+       *    @CodeForces : iAmrifat
+       *    @UVA        : rifat_ikbal
+       *    @facebook   : https://www.facebook.com/ikbal.rifat.5
+       */
+ 
+      /* ll dx[] = {1,-1,0,0} , dy[] = {0,0,1,-1}; */ // 4 Direction
+      /* ll dx[] = {1,-1,0,0,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1,1,-1}; */ // 8 Direction
+      /* ll dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; */ // Knight Direction
+      /* ll dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; */ // Hexagonal Direction
+ 
+}
+ 
+ 
