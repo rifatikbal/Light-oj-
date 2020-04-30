a3597edfaa67ae8034798a30de5d4923b10e34bf
Rifat ikbal <ikhossain991@gmail.com>
2020-04-30 16:35:28 +0600

    Add files via upload

--- 1326.cpp
+++ 1326.cpp
@@ -15,13 +15,13 @@
 #include <queue>
  
 using namespace std;
-
+ 
 #define ll long long
 #define ulli  unsigned ll int
 #define sl(n) scanf("%lld", &n)
 #define sl2(a,b) scanf("%lld%lld", &a,&b)
 #define sl3(a,b,c) scanf("%lld%lld%lld", &a,&b,&c)
-
+ 
 #define si(n) scanf("%d", &n)
 #define si2(a,b) scanf("%d%d", &a,&b)
 #define si3(a,b,c) scanf("%d%d%d", &a,&b,&c)
@@ -48,7 +48,7 @@ using namespace std;
 #define  bug2(a,b)  cout<<a<<" "<<b<<endl;
 #define  bug3(a,b,c)  cout<<a<<" "<<b<<" "<<c<<endl;
 #define  bug4(a,b,c,d)  cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
-
+ 
 #define  tst(n)  for(int cs=1;cs<=n;cs++)
 #define spc <<" "<<
 #define  pys puts("YES");
@@ -58,97 +58,97 @@ using namespace std;
 #define zero puts("0");
 #define limit 1001
 #define md 10056
-
-
+ 
+ 
 typedef pair<ll,ll> iPair;
-
+ 
   int chos[limit][limit],dp[limit];
-
-
-
-
+ 
+ 
+ 
+ 
  int solve(int n)
  {
-
+ 
   if(n==0||n==1)return dp[1]=dp[0]=1;
-
-
+ 
+ 
   int & ret=dp[n];
-
+ 
   if(ret!=-1)return ret;
-
+ 
   int s=0;
   for(int i=1;i<=n;i++)
   {
-
-
+ 
+ 
    s=s%md+((chos[n][i]%md)*(solve(n-i)%md))%md;
    s%=md;
   }
-
+ 
   return dp[n]=s;
  }
-
-
+ 
+ 
 int main()
 {
  
                
      /*  freopen("input.txt","r",stdin);
        freopen("output.txt","w",stdout);*/
-
-
+ 
+ 
                for(int i=0;i<=1000;i++)chos[i][0]=1;
-
+ 
                 chos[1][1]=1;
                for(int i=2;i<=1000;i++)
-               { 
-
-
+               {
+ 
+ 
                 for(int j=1;j<=i;j++)
                 {
-
+ 
                   chos[i][j]=chos[i-1][j]%md+chos[i-1][j-1]%md;
-
+ 
                   chos[i][j]%=md;
                 }
                }
                   memset(dp,-1,sizeof dp);
-
+ 
                  solve(1000);
-                  
+                 
            /*       fr(i,1,10)
                   {
-
+ 
                     fr(j,1,i)cout<<" "<<i<<" "<<j<<" "<<chos[i][j]<<"\n";
-
+ 
                   }*/
                  
-
+ 
                ll test;
                cin>>test;
                tst(test)
                {
-
-
+ 
+ 
                 int n;
-
+ 
                 si(n);
-
+ 
                 printf("Case %d: %d\n",cs,dp[n]);
-
+ 
                 //fr(i,1,n)sl(A[i]);
                    
                }
-      
-
-
-
-
-    
-
+     
  
-
+ 
+ 
+ 
+   
+ 
+ 
+ 
         /**
        *    @author     : Ikbal Hossain
        *    @University  : RUET CSE 15
