e24589107c1f02c8f67d1bea5a8c298cf779556e
Rifat ikbal <ikhossain991@gmail.com>
2020-05-04 04:42:59 +0600

    Add files via upload

---
+++ 1071.cpp
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <iostream>
+#include <climits>
+#include <map>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <vector>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <utility>
+#include <queue>
+ 
+using namespace std;
+ 
+#define ll long long
+#define ulli  unsigned ll int
+#define sl(n) scanf("%lld", &n)
+#define sl2(a,b) scanf("%lld%lld", &a,&b)
+#define sl3(a,b,c) scanf("%lld%lld%lld", &a,&b,&c)
+#define VI  vector<int>
+#define Vll  vector<ll>
+#define   all(v)  v.begin(),v.end()
+#define ub(v,x)  upper_bound(all(v),x)
+ 
+bool check(int x,int pos){return  (bool)(x &(1<<pos));}
+int st(int x,int pos){  return x= x |(1<<pos);}
+int  Reset(int x,int pos ){ return x= x &~(1<<pos);}
+ 
+#define si(n) scanf("%d", &n)
+#define si2(a,b) scanf("%d%d", &a,&b)
+#define si3(a,b,c) scanf("%d%d%d", &a,&b,&c)
+#define sf(n) scanf("%lf", &n)
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define plp pair <int, pll >
+#define pb push_back
+#define mkpr  make_pair
+#define   ff   first
+#define   ss  second
+#define  srt(V)  sort(V.begin(),V.end());
+#define nl printf("\n");
+#define  PI  2*acos(0.0);
+#define  INFP ((ll)1<<62)
+#define  INFN (0-(ll)1<<62)
+#define  loop(a,b)  for(int g=a;g<=b;g++)
+#define  for1(n)  for(int i=0;i<n;i++)
+#define  for2(n)  for(int j=0;j<n;j++)
+#define  for3(n)  for(int k=0;k<n;k++)
+#define  for4(n)  for(int l=0;l<n;l++)
+#define fr(i,a,b) for(int i=a;i<=b;i++)
+#define  bug1(a)  cout<<a<<endl;
+#define  bug2(a,b)  cout<<a<<" "<<b<<endl;
+#define  bug3(a,b,c)  cout<<a<<" "<<b<<" "<<c<<endl;
+#define  bug4(a,b,c,d)  cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+ 
+#define  tst(n)  for(int cs=1;cs<=n;cs++)
+#define spc <<" "<<
+#define  pys puts("YES");
+#define  pno  puts("NO");
+#define bug puts("bug");
+#define  pmn puts("-1");
+#define zero puts("0");
+#define limit 200006
+#define md 1000000007
+ 
+ 
+ 
+ 
+typedef pair<int,int> iPair;
+ 
+ 
+int A[101][101],dp[101][101][101],n,m;
+ 
+ 
+ 
+int solve(int x1,int y1,int x2,int y2)
+{
+ 
+      int & ret=dp[x1][y1][x2];
+ 
+      if(x1==x2&&y1==y2)return 0;
+ 
+      if(ret!=-1)return ret;
+ 
+      if(x1>n||y1>m||x2>n||y2>m)return 0;
+      int mx=0;
+      int x=solve(x1+1,y1,x2+1,y2);
+      mx=max(x,mx);
+      x=solve(x1+1,y1,x2,y2+1);
+      mx=max(mx,x);
+      x=solve(x1,y1+1,x2,y2+1);
+      mx=max(mx,x);
+      x=solve(x1,y1+1,x2+1,y2);
+      mx=max(mx,x);
+ 
+ 
+     return ret=mx+A[x1][y1]+A[x2][y2];
+ 
+}
+ 
+int main()
+{
+ 
+    /*          
+       freopen("input.txt","r",stdin);
+       freopen("output.txt","w",stdout);*/
+ 
+                int test;
+                cin>>test;
+                tst(test)
+                {  
+                  memset(dp,-1,sizeof dp);
+ 
+                   //int n,m;
+ 
+                   si2(n,m);
+ 
+                   fr(i,1,n)
+                   {
+ 
+                    fr(j,1,m)si(A[i][j]);
+                   }
+ 
+                   int ans=solve(2,1,1,2);
+ 
+ 
+                   printf("Case %d: %d\n",cs,ans+A[1][1]+A[n][m]);
+ 
+ 
+ 
+                   
+                }
+       
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+        /**
+       *    @author     : Ikbal Hossain
+       *    @University  : RUET CSE 15
+       *    @CodeForces : iAmrifat
+       *    @UVA        : rifat_ikbal
+       *    @facebook   : https://www.facebook.com/ikbal.rifat.5
+       */
+ 
+      /* ll dx[] = {1,-1,0,0} , dy[] = {0,0,1,-1}; */ // 4 Direction
+      /* ll dx[] = {1,-1,0,0,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1,1,-1}; */ // 8 Direction
+      /* ll dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; */ // Knight Direction
+      /* ll dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; */ // Hexagonal Direction
+ 
+}
+ 
+ 
+ 
+ 
+ 
