6ecd6873894f12faf0b5e8ba8d959544fe1f29f2
Rifat ikbal <ikhossain991@gmail.com>
2021-04-08 17:04:09 +0600

    Update 1128.cpp

--- 1128.cpp
+++ 1128.cpp
@@ -66,237 +66,112 @@ int  Reset(int x,int pos ){ return x= x &~(1<<pos);}
 #define zero puts("0");
 #define limit 100001
 #define md 1000000007
- 
- 
- 
- 
-   typedef pair<int,int> iPair;
-   int par[limit][20],pmx[limit][20],n,lavel[limit];
- 
-   VI graph[limit];
- 
-   void  sparse(int n)
-   {
- 
+typedef pair<int,int> iPair;
+int par[limit][20],pmx[limit][20],n,lavel[limit];
+VI graph[limit];
+
+void  sparse(int n)
+{
    for(int j=1;(1<<j)<=n;j++)
    {
- 
       for(int i=0;i<n;i++)
       {      
- 
         if(par[i][j-1]==-1)continue;
- 
         par[i][j]=par[par[i][j-1]][j-1];
- 
         pmx[i][j]=max(pmx[i][j-1],pmx[par[i][j-1]][j-1]);
- 
- 
       }
- 
    }
- 
- 
-   }
- 
- 
-   int query(int x,int y)
-   {
- 
- 
-    int l=0,h=lavel[x]-1;
- 
-    //cout<<h<<endl;
- 
- 
-    while(l<h)
-    {          
- 
-             // cout<<l<<" aa "<<h<<endl;
- 
- 
-                int m=(l+h+1)/2;
- 
-                int p=m;
-                int xx=x;
-             
- 
-                for(int i=log2(n)+1;i>=0;i--)
-                {
-                 
- 
-                  if((1<<i)<=p)
-                  {
- 
-                    //cout<<i<<endl;
- 
-                    xx=par[xx][i];
- 
-                    p-=(1<<i);
-                  }
- 
-                }
- 
-               //cout<<xx<<" yy "<<p<<endl;
- 
-                p=lavel[xx];
-                int mx=0;
-               
- 
-                 for(int i=log2(n)+1;i>=0;i--)
-                {
- 
-                  if((1<<i)<=p)
-                  {
- 
-                 //   cout<<i<<endl;
- 
-                    mx=max(mx,pmx[xx][i]);
- 
-                    xx=par[xx][i];
- 
-                    p-=(1<<i);
-                  }
- 
-                }
- 
-                //cout<<mx<<" dd "<<p<<endl;
- 
-                if(mx<y)
-                {
- 
-                  h=m-1;
-                }
-                else
-                {
- 
-                  l=m;
-                }
- 
-               // cout<<l<<" sss "<<h<<endl;
-               
-     }
- 
-     //cout<<l<<" ss "<<endl;
- 
-              int p=l;
- 
-             
- 
- 
-                 for(int i=log2(n)+1;i>=0;i--)
-                {
- 
-                  if((1<<i)<=p)
-                  {
- 
-                    x=par[x][i];
-                    p-=(1<<i);
-                  }
- 
-                }
- 
-                //cout<<x<<endl;
- 
-                return x;
- 
- 
- 
- 
- 
-   }
- 
- 
- 
-   void dfs(int u,int pr)
-   {
- 
- 
-    for(int i=0;i<graph[u].size();i++)
-    {
- 
-      int v=graph[u][i];
- 
-      if(v==pr)continue;
-         lavel[v]=lavel[u]+1;
-      dfs(v,u);
-    }
-   }
- 
- 
-int main()
+}
+
+int query(int x,int y)
 {
- 
-               
+  int l=0,h=lavel[x]-1;
+  while(l<h)
+  {
+      int m=(l+h+1)/2;
+      int p=m;
+      int xx=x;
+      for(int i=log2(n)+1;i>=0;i--)
+      {
+        if((1<<i)<=p)
+        {
+          xx=par[xx][i];
+          p-=(1<<i);
+        }
+      }
+      p=lavel[xx];
+      int mx=0;
+      for(int i=log2(n)+1;i>=0;i--)
+      {
+        if((1<<i)<=p)
+        {
+          mx=max(mx,pmx[xx][i]);
+          xx=par[xx][i];
+          p-=(1<<i);
+        }
+       }
+       if(mx<y)h=m-1;
+       else l=m;
+  }
+  int p=l;
+  for(int i=log2(n)+1;i>=0;i--)
+  {
+     if((1<<i)<=p)
+     {
+        x=par[x][i];
+        p-=(1<<i);
+     }
+  }
+  return x;
+} 
+void dfs(int u,int pr)
+{
+  for(int i=0;i<graph[u].size();i++)
+  {
+    int v=graph[u][i];
+    if(v==pr)continue;
+    lavel[v]=lavel[u]+1;
+    dfs(v,u);
+  }
+}
+
+int main()
+{     
      /*  freopen("input.txt","r",stdin);
        freopen("output.txt","w",stdout);*/
- 
-                int test;
-                cin>>test;
-                tst(test)
-                {    
-                   
-                    memset(pmx,0,sizeof pmx);
-                    memset(par,-1,sizeof par);
- 
-                    memset(lavel,0,sizeof lavel);
-                    int m;
- 
-                    si2(n,m);
-                    graph[0].clear();
- 
-                    for(int i=1;i<n;i++)
-                    {
- 
-                      int a,b;
- 
-                      si2(a,b);
- 
-                      graph[a].pb(i);
-                      graph[i].pb(a);
-                   
- 
-                      par[i][0]=a;
-                      pmx[i][0]=b;
-                      graph[i].clear();
-                    }
-                    pmx[0][0]=1;
- 
-                      lavel[0]=1;
-                     dfs(0,-1);
-                    sparse(n);
- 
-                    //cout<<lavel[5]<<endl;
- 
-                   
- 
-                    printf("Case %d:\n",cs);
-                 
- 
-                    fr(i,1,m)
-                    {
-                     
-                      int x,y;
-                      si2(x,y);
- 
-                      printf("%d\n",query(x,y));
- 
- 
-                    }
- 
-                   
-                }
- 
-                //cout<<pmx[0][0]<<endl;
-       
- 
- 
- 
- 
- 
-   
- 
- 
- 
+      int test;
+      cin>>test;
+      tst(test)
+      {    
+        memset(pmx,0,sizeof pmx);
+        memset(par,-1,sizeof par);
+        memset(lavel,0,sizeof lavel);
+        int m;
+        si2(n,m);
+        graph[0].clear();
+        for(int i=1;i<n;i++)
+        {
+          int a,b;
+          si2(a,b);
+          graph[a].pb(i);
+          graph[i].pb(a);
+          par[i][0]=a;
+          pmx[i][0]=b;
+          graph[i].clear();
+        }
+        pmx[0][0]=1;
+        lavel[0]=1;
+        dfs(0,-1);
+        sparse(n);
+        printf("Case %d:\n",cs);
+        fr(i,1,m)
+        {
+          int x,y;
+          si2(x,y);
+          printf("%d\n",query(x,y));
+        }                   
+     }
+
         /**
        *    @author     : Ikbal Hossain
        *    @University  : RUET CSE 15
